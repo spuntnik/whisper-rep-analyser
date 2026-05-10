114e4698401bb4fc25941920bfd87dcfd45432d5
Wei An Mark Tay <weianmarktay@Weis-MacBook-Pro.local>
2026-05-10 09:00:41 +0800

    Force dynamic root page rendering

--- predicate-analyzer/app/page.tsx
+++ predicate-analyzer/app/page.tsx
@@ -1,5 +1,8 @@
 import { PredicateAnalyzerApp } from "@/components/PredicateAnalyzerApp";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function Page() {
   return <PredicateAnalyzerApp />;
 }
