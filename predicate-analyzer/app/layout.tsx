b25fd07f1cfb9002010fdc4e537684e032de86c3
Wei An Mark Tay <weianmarktay@Weis-MacBook-Pro.local>
2026-05-10 09:49:15 +0800

    Make stable stylesheet link render in body

--- predicate-analyzer/app/layout.tsx
+++ predicate-analyzer/app/layout.tsx
@@ -14,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <link rel="stylesheet" href="/app.css" />
+        {children}
+      </body>
     </html>
   );
 }
